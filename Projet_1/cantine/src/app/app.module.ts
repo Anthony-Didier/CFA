dd97eaebc1049a58c74a6277c2b4c00c4f66928b
tony91700 <didieranthony1@gmail.com>
2020-12-04 18:36:30 +0100

    ajout confirmation d'annulation commande

--- Projet_1/cantine/src/app/app.module.ts
+++ Projet_1/cantine/src/app/app.module.ts
@@ -5,21 +5,27 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { HttpClientModule } from '@angular/common/http';
+import { MatDialogModule } from '@angular/material/dialog';
 import { MatCardModule } from '@angular/material/card';
 import { RecapCommandeComponent } from './pages/recap-commande/recap-commande.component';
 import { RecapCommandesPasseesComponent } from './pages/recap-commandes-passees/recap-commandes-passees.component';
+import { ConfirmDialogComponent } from './pages/confirm-dialog/confirm-dialog.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
     RecapCommandeComponent,
-    RecapCommandesPasseesComponent
+    RecapCommandesPasseesComponent,
+    ConfirmDialogComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    MatCardModule
+    MatDialogModule,
+    MatCardModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Projet_1/cantine/src/app/pages/recap-commandes-passees/recap-commandes-passees.component.ts
+++ Projet_1/cantine/src/app/pages/recap-commandes-passees/recap-commandes-passees.component.ts
@@ -1,30 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { CantiniereService } from '../../../service/cantiniere.service';
 import { Commande } from '../../models/Commande';
+import { ConfirmDialogComponent } from '../confirm-dialog/confirm-dialog.component';
 
 @Component({
   selector: 'app-recap-commandes-passees',
   templateUrl: './recap-commandes-passees.component.html',
-  styleUrls: ['./recap-commandes-passees.component.css']
+  styleUrls: ['./recap-commandes-passees.component.css'],
 })
 export class RecapCommandesPasseesComponent implements OnInit {
-    title = 'angular-cantinere';
-    commandes_passees: Commande[] = [];
+  title = 'angular-cantinere';
+  commandes_passees: Commande[] = [];
 
-  constructor(private cantiniere_api : CantiniereService) {
-    this.cantiniere_api.findAll().subscribe(data => {
+  constructor(private cantiniere_api: CantiniereService, private dialog: MatDialog) {
+    this.cantiniere_api.findAll().subscribe((data) => {
       this.commandes_passees = data;
       console.log(this.commandes_passees);
-    })
+    });
   }
 
   cancelOrder(id: number) {
-    
-    this.cantiniere_api.cancel(id).subscribe(() => window.location.reload())
-    // console.log("Commande n°" + id + " annulée !");
+    const dialogRef = this.dialog.open(ConfirmDialogComponent, {
+      data: {
+        title: "Annulation de la commande",
+        message: "Confirmer l'annulation de la commande n°" + id + " ?"
+      }
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      if (result == 'confirm') {
+        this.cantiniere_api.cancel(id).subscribe(() => window.location.reload());
+        // console.log("Commande n°" + id + " annulée !");
+      }
+    });
   }
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
