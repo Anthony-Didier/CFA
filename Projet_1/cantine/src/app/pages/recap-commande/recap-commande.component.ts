beb0787788be38f3f4570af0a89fa5f5dcd86731
tony91700 <didieranthony1@gmail.com>
2020-12-17 16:35:38 +0100

    amélioration fonctionnalités

--- Projet_1/cantine/src/app/pages/recap-commande/recap-commande.component.ts
+++ Projet_1/cantine/src/app/pages/recap-commande/recap-commande.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { MatDialog } from '@angular/material/dialog';
 import { CantiniereService } from '../../../service/cantiniere.service';
 import { Commande } from '../../models/Commande';
 import { User } from '../../models/User';
+import { ConfirmDialogComponent } from '../confirm-dialog/confirm-dialog.component';
 
 @Component({
   selector: 'app-recap-commande',
@@ -17,12 +19,28 @@ export class RecapCommandeComponent implements OnInit {
 
   constructor(
     private cantiniere_api: CantiniereService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute, 
+    private dialog: MatDialog
   ) {
     this.userId = this.route.snapshot.paramMap.get("id");
     this.cantiniere_api.findAllbyUser(this.userId).subscribe((data) => {
       this.commandes = data;
       console.log(this.commandes);
+    });
+  }
+
+  cancelOrder(id: number) {
+    const dialogRef = this.dialog.open(ConfirmDialogComponent, {
+      data: {
+        title: "Annulation de la commande",
+        message: "Confirmer l'annulation de la commande n°" + id + " ?"
+      }
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      if (result == 'confirm') {
+        this.cantiniere_api.cancel(id).subscribe(() => window.location.reload());
+        // console.log("Commande n°" + id + " annulée !");
+      }
     });
   }
 
